a6b4aa008ce7b920a136d50a34af7866318db103
ponder <ponder>
2001-05-06 18:57:45 +0000

    Added dnd support files.

---
+++ freemind/freemind/controller/NodeDragListener.java
@@ -0,0 +1,78 @@
+/*FreeMind - A Program for creating and viewing Mindmaps
+ *Copyright (C) 2000-2001  Joerg Mueller <joergmueller@bigfoot.com>
+ *See COPYING for Details
+ *
+ *This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package freemind.controller;
+
+import freemind.modes.MindMapNode;
+import freemind.view.mindmapview.NodeView;
+// Drag & Drop
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DnDConstants;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DragSourceListener;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceEvent;
+
+/**
+ * The NodeDragListener which belongs to every
+ * NodeView
+ */
+public class NodeDragListener implements DragGestureListener {
+
+    private final Controller c;
+
+    public NodeDragListener(Controller controller) {
+	c = controller;
+    }
+
+    public void dragGestureRecognized(DragGestureEvent e) {
+	MindMapNode node = ((NodeView)e.getComponent()).getModel();
+	System.out.println("Node:"+node);
+	
+	if (node.isRoot()) return;
+	Transferable t = c.getModel().cut(node);
+	System.out.println("Trans:"+t);
+	
+	// starts the dragging
+	DragSource dragSource = DragSource.getDefaultDragSource();
+	dragSource.startDrag (e, DragSource.DefaultMoveDrop, t,
+			      new DragSourceListener() {
+				      public void	dragDropEnd(DragSourceDropEvent dsde) {
+					  // if not ok, go back
+					  if ( !dsde.getDropSuccess()){
+					      MindMapNode oldnode = ((NodeView)dsde.getDragSourceContext().getComponent()).getModel();
+					      c.getModel().paste(oldnode,(MindMapNode)oldnode.getParent());
+					  }
+				      }
+				      public void	dragEnter(DragSourceDragEvent dsde) {
+				      }
+				      public void	dragExit(DragSourceEvent dse) {
+				      }
+				      public void	dragOver(DragSourceDragEvent dsde) {
+				      }
+				      public void	dropActionChanged(DragSourceDragEvent dsde) {
+				      }
+				  }
+			      );
+    }
+}
+
---
+++ freemind/freemind/controller/NodeDropListener.java
@@ -0,0 +1,68 @@
+/*FreeMind - A Program for creating and viewing Mindmaps
+ *Copyright (C) 2000-2001  Joerg Mueller <joergmueller@bigfoot.com>
+ *See COPYING for Details
+ *
+ *This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package freemind.controller;
+
+import freemind.view.mindmapview.NodeView;
+import freemind.view.mindmapview.MapView;
+import freemind.modes.MindMapNode;
+// For Drag&Drop
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+
+public class NodeDropListener implements DropTargetListener {
+
+    private final Controller c;
+
+    public NodeDropListener(Controller controller) {
+	c = controller;
+    }
+	
+    public void drop (DropTargetDropEvent dtde) {
+      dtde.acceptDrop(DnDConstants.ACTION_MOVE);
+      try {
+        Transferable t = dtde.getTransferable();
+		MindMapNode node = ((NodeView)dtde.getDropTargetContext().getComponent()).getModel();
+		c.getModel().paste(t,node);
+      }
+      catch (Exception e) {
+        dtde.dropComplete(false);
+        return;
+      }
+      dtde.dropComplete(true);
+    }
+
+    public void dragEnter (DropTargetDragEvent dtde) {
+		// TODO: check DataFlavor before say ok
+		// then dtde.rejectDrag(); if not
+		dtde.acceptDrag(DnDConstants.ACTION_MOVE);
+    }
+
+    public void dragOver (DropTargetDragEvent e) {}
+    public void dragExit (DropTargetEvent e) {}
+    public void dragScroll (DropTargetDragEvent e) {}
+    public void dropActionChanged (DropTargetDragEvent e) {}    
+}
+
