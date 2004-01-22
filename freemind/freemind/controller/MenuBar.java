199dd9374e1abe8a70286ec0f466e1d4b3fe3c45
christianfoltin <christianfoltin>
2004-01-22 06:03:54 +0000

    Changes for 0.7.1 RC2

--- freemind/freemind/controller/MenuBar.java
+++ freemind/freemind/controller/MenuBar.java
@@ -16,7 +16,7 @@
  *along with this program; if not, write to the Free Software
  *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/*$Id: MenuBar.java,v 1.22 2004-01-17 23:20:57 christianfoltin Exp $*/
+/*$Id: MenuBar.java,v 1.23 2004-01-22 06:03:54 christianfoltin Exp $*/
 
 package freemind.controller;
 
@@ -242,8 +242,8 @@ public class MenuBar extends JMenuBar {
 
 	preferences.addSeparator();
     addOptionSet( c.optionSelectionMechanismAction,
-                  new String[]{ "selection_method_delayed",
-                                "selection_method_direct",
+                  new String[]{ "selection_method_direct",
+                                "selection_method_delayed",
                                 "selection_method_by_click"},
                   preferences, c.getProperty("selection_method") );
 
