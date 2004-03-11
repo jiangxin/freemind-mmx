083a3a5a892d1c128462b4fead9db2dbd1e1d65e
christianfoltin <christianfoltin>
2004-03-11 06:40:48 +0000

    Icon selection plugin added

---
+++ freemind/accessories/plugins/IconSelectionPlugin.java
@@ -0,0 +1,114 @@
+/*
+ * Created on 06.03.2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package accessories.plugins;
+
+import java.awt.Point;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.Action;
+
+import accessories.plugins.dialogs.IconSelectionPopupDialog;
+
+import freemind.extensions.NodeHookAdapter;
+import freemind.main.FreeMind;
+import freemind.modes.MindIcon;
+import freemind.modes.MindMap;
+import freemind.modes.MindMapNode;
+import freemind.modes.ModeController;
+import freemind.modes.mindmapmode.MindMapController;
+import freemind.modes.mindmapmode.MindMapController.IconAction;
+import freemind.view.mindmapview.NodeView;
+
+/**
+ * @author ganzer
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class IconSelectionPlugin extends NodeHookAdapter {
+
+	private MindIcon icon;
+
+	/**
+	 * @param node
+	 * @param map
+	 * @param controller
+	 */
+	public IconSelectionPlugin() {
+		super();
+	}
+	
+	/* (non-Javadoc)
+	 * @see freemind.extensions.NodeHook#invoke(freemind.modes.MindMapNode, java.util.List)
+	 */
+	public void invoke(MindMapNode focussed, List selecteds) {
+		Vector items = new Vector();
+		Vector itemdescriptions = new Vector();
+		MindMapController controller = ((MindMapController) getController());
+		Vector iconActions = controller.iconActions;
+		for (Enumeration e = iconActions.elements(); e.hasMoreElements();) {
+			IconAction action =
+				((MindMapController.IconAction) e.nextElement());
+			items.add(action.getValue(Action.SMALL_ICON));
+			itemdescriptions.add(action.getValue(Action.SHORT_DESCRIPTION));
+		}
+
+		FreeMind frame = (FreeMind) getController().getFrame();
+		IconSelectionPopupDialog selectionDialog =
+			new IconSelectionPopupDialog(
+				frame,
+				items,
+				itemdescriptions,
+				frame);
+
+		NodeView node = focussed.getViewer();
+		// this code is copied from ControllerAdapter, edit
+		//URGENT: DO NOT COPY CODE!
+		controller.getView().scrollNodeToVisible(node, 0);
+		Point frameScreenLocation =
+			frame.getLayeredPane().getLocationOnScreen();
+		double posX =
+			node.getLocationOnScreen().getX() - frameScreenLocation.getX();
+		double posY =
+			node.getLocationOnScreen().getY() - frameScreenLocation.getY() + 20;
+		if (posX + selectionDialog.getWidth()
+			> frame.getLayeredPane().getWidth()) {
+			posX =
+				frame.getLayeredPane().getWidth() - selectionDialog.getWidth();
+		}
+		if (posY + selectionDialog.getHeight()
+			> frame.getLayeredPane().getHeight()) {
+			posY =
+				frame.getLayeredPane().getHeight()
+					- selectionDialog.getHeight();
+		}
+		posX = ((posX < 0) ? 0 : posX) + frameScreenLocation.getX();
+		posY = ((posY < 0) ? 0 : posY) + frameScreenLocation.getY();
+		selectionDialog.setLocation(
+			new Double(posX).intValue(),
+			new Double(posY).intValue());
+		selectionDialog.setModal(true);
+		selectionDialog.show();
+		// process result:
+		this.icon = ((IconAction) iconActions.get(selectionDialog.getResult())).icon;
+		// and the super method:
+		super.invoke(focussed, selecteds);
+	}
+
+	/* (non-Javadoc)
+	 * @see freemind.extensions.NodeHook#invoke()
+	 */
+	public void invoke(MindMapNode node) {
+		setNode(node);
+		node.addIcon(icon);
+		nodeChanged(node);
+	}
+
+
+}
