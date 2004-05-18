213e07d7bce267bc8470854c97357200ddc569c2
christianfoltin <christianfoltin>
2004-05-18 18:58:53 +0000

    bug fixed

--- freemind/freemind.bat
+++ freemind/freemind.bat
@@ -1,1 +1,2 @@
-java -cp .;lib/jython.jar;lib/freemind.jar;${full}/lib/ant/lib/jaxb-api.jar;${full}/lib/ant/lib/jaxb-impl.jar;${full}/lib/ant/lib/jaxb-libs.jar;${full}/lib/ant/lib/namespace.jar;${full}/lib/ant/lib/relaxngDatatype.jar;${full}/lib/ant/lib/xsdlib.jar;${full}/lib/ant/lib/jax-qname.jar;${full}/lib/ant/lib/sax.jar;${full}/lib/ant/lib/dom.jar freemind.main.FreeMind
+@echo off
+start javaw -cp .;lib\jython.jar;lib\freemind.jar;lib\ant\lib\jaxb-api.jar;lib\ant\lib\jaxb-impl.jar;lib\ant\lib\jaxb-libs.jar;lib\ant\lib\namespace.jar;lib\ant\lib\relaxngDatatype.jar;lib\ant\lib\xsdlib.jar;lib\ant\lib\jax-qname.jar;lib\ant\lib\sax.jar;lib\ant\lib\dom.jar freemind.main.FreeMind
