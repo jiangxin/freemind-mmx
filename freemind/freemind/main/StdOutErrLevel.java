fc3b662c164e13d2932ebdb286528db187444516
dpolivaev <dpolivaev>
2007-09-15 22:19:49 +0000

    no message

--- freemind/freemind/main/StdOutErrLevel.java
+++ freemind/freemind/main/StdOutErrLevel.java
@@ -1,12 +1,4 @@
 package freemind.main;
-
-/*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
- *
- * ident        "$Revision: 1.1.2.1 $      SMI"
- */
-
 import java.io.InvalidObjectException;
 import java.io.ObjectStreamException;
 import java.util.logging.Level;
@@ -15,14 +7,11 @@ import java.util.logging.Level;
  * Class defining 2 new Logging levels, one for STDOUT, one for STDERR,
  * used when multiplexing STDOUT and STDERR into the same rolling log file
  * via the Java Logging APIs.
- * @since 2.2
  */
 public class StdOutErrLevel extends Level {
-    
+   
     /**
-     * private constructor
-     * @param name name used in toString
-     * @param value integer value, should correspond to something reasonable in default Level class
+     * Private constructor
      */
     private StdOutErrLevel(String name, int value) {
         super(name, value);
@@ -30,11 +19,13 @@ public class StdOutErrLevel extends Level {
     /**
      * Level for STDOUT activity.
      */
-    public static Level STDOUT = new StdOutErrLevel("STDOUT", Level.INFO.intValue()+53);
+    public static Level STDOUT =
+        new StdOutErrLevel("STDOUT", Level.INFO.intValue()+53);
     /**
      * Level for STDERR activity
      */
-    public static Level STDERR = new StdOutErrLevel("STDERR", Level.SEVERE.intValue()+53);
+    public static Level STDERR =
+        new StdOutErrLevel("STDERR", Level.SEVERE.intValue()+53);
 
     /**
      * Method to avoid creating duplicate instances when deserializing the
@@ -44,12 +35,11 @@ public class StdOutErrLevel extends Level {
      * @throws ObjectStreamException If unable to deserialize
      */
     protected Object readResolve()
-	throws ObjectStreamException {
+        throws ObjectStreamException {
         if (this.intValue() == STDOUT.intValue())
             return STDOUT;
         if (this.intValue() == STDERR.intValue())
             return STDERR;
         throw new InvalidObjectException("Unknown instance :" + this);
-    }        
-    
+    }       
 }
