6c579cf8bbed9c511ea3dbace2ee5948b061b550
dpolivaev <dpolivaev>
2008-05-26 20:51:22 +0000

    Use standard options with mnemonics.  for yes/no/cancel option dialog.

--- freemind/freemind/modes/ControllerAdapter.java
+++ freemind/freemind/modes/ControllerAdapter.java
@@ -16,7 +16,7 @@
  *along with this program; if not, write to the Free Software
  *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/* $Id: ControllerAdapter.java,v 1.41.14.37.2.45 2008-04-25 20:10:38 christianfoltin Exp $ */
+/* $Id: ControllerAdapter.java,v 1.41.14.37.2.46 2008-05-26 20:51:22 dpolivaev Exp $ */
 
 package freemind.modes;
 
@@ -722,14 +722,11 @@ public abstract class ControllerAdapter implements ModeController {
      * Return false if user has canceled.
      */
     public boolean close(boolean force, MapModuleManager mapModuleManager) {
-        String[] options = {Tools.removeMnemonic(getText("yes")),
-        		Tools.removeMnemonic(getText("no")),
-        		Tools.removeMnemonic(getText("cancel"))};
         if (!force && !getModel().isSaved()) {
             String text = getText("save_unsaved")+"\n"+mapModuleManager.getMapModule().toString();
             String title = Tools.removeMnemonic(getText("save"));
             int returnVal = JOptionPane.showOptionDialog(getFrame().getContentPane(),text,title,JOptionPane.YES_NO_CANCEL_OPTION,
-                                                         JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
+                                                         JOptionPane.QUESTION_MESSAGE,null,null,null);
             if (returnVal==JOptionPane.YES_OPTION) {
                boolean savingNotCancelled = save();
                if (!savingNotCancelled) {
