86250f21f2a7e8cd1c3248c114f1594572938467
christianfoltin <christianfoltin>
2010-09-22 20:13:55 +0000

    * New version number * Derived size in note editor is now the default

--- freemind/freemind/main/FreeMind.java
+++ freemind/freemind/main/FreeMind.java
@@ -16,7 +16,7 @@
  *along with this program; if not, write to the Free Software
  *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/*$Id: FreeMind.java,v 1.32.14.28.2.139 2010-07-18 10:33:00 christianfoltin Exp $*/
+/*$Id: FreeMind.java,v 1.32.14.28.2.140 2010-09-22 20:13:55 christianfoltin Exp $*/
 
 package freemind.main;
 
@@ -128,7 +128,7 @@ public class FreeMind extends JFrame implements FreeMindMain {
 
 	private Logger logger = null;
 	
-	protected static final VersionInformation VERSION = new VersionInformation("0.9.0 RC 9");
+	protected static final VersionInformation VERSION = new VersionInformation("0.9.0 RC 10");
 	
 	public static final String XML_VERSION = "0.9.0";
 
