731808e65bbf907d87d71643e049c6866590db3a
Christian Foltin <christianfoltin@users.sourceforge.net>
2011-05-28 21:39:42 +0200

    * little stuff

--- freemind/freemind/controller/Controller.java
+++ freemind/freemind/controller/Controller.java
@@ -758,7 +758,7 @@ public class Controller  implements MapModuleChangeObserver {
     public void obtainFocusForSelected() {
 //    	logger.finest("obtainFocusForSelected");
     	if (getView() != null) { // is null if the last map was closed.
-    		logger.info("Requesting Focus for " + getView().getSelected());
+    		logger.fine("Requesting Focus for " + getView().getSelected());
     		getView().getSelected().requestFocus();
     	} else {
     		// fc, 6.1.2004: bug fix, that open and quit are not working if no map is present.
