fbe92956322729d73a6628bb1015a9fd067b4362
Christian Foltin <christianfoltin@users.sourceforge.net>
2013-02-10 21:55:01 +0100

    * Added undo support for inine node editing (also fix for https://sourceforge.net/tracker/?func=detail&aid=3603909&group_id=7118&atid=107118)

--- freemind/freemind/view/mindmapview/EditNodeTextField.java
+++ freemind/freemind/view/mindmapview/EditNodeTextField.java
@@ -28,6 +28,8 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.FocusEvent;
@@ -37,9 +39,14 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.AbstractAction;
 import javax.swing.JComponent;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 import com.inet.jortho.SpellChecker;
 
@@ -54,44 +61,54 @@ import freemind.modes.ModeController;
  * 
  */
 public class EditNodeTextField extends EditNodeBase {
+	final int EDIT = 1;
+	final int CANCEL = 2;
+	int cursorWidth = 1;
+	int xOffset = 0;
+	int yOffset = -1; // Optimized for Windows style; basically ad hoc
+	int widthAddition = 2 * 0 + cursorWidth + 2;
+	int heightAddition = 2;
+
+	// minimal width for input field of leaf or folded node (PN)
+	final int MINIMAL_LEAF_WIDTH = 150;
+	final int MINIMAL_WIDTH = 50;
+	final int MINIMAL_HEIGHT = 20;
+
 	private KeyEvent firstEvent;
 	protected JTextField textfield;
 	protected JComponent mParent;
 	private final JComponent mFocusListener;
+	protected static java.util.logging.Logger logger = null;
+	private Tools.IntHolder mEventSource;
+	private UndoManager mUndoManager;
 
 	public EditNodeTextField(final NodeView node, final String text,
 			final KeyEvent firstEvent, ModeController controller,
 			EditControl editControl) {
-		this(node, text, firstEvent, controller, editControl, node.getMap(), node);
+		this(node, text, firstEvent, controller, editControl, node.getMap(),
+				node);
 	}
 
 	public EditNodeTextField(final NodeView node, final String text,
 			final KeyEvent firstEvent, ModeController controller,
-			EditControl editControl, JComponent pParent, JComponent pFocusListener)
-	{
+			EditControl editControl, JComponent pParent,
+			JComponent pFocusListener) {
 		super(node, text, controller, editControl);
 		this.firstEvent = firstEvent;
 		mParent = pParent;
 		mFocusListener = pFocusListener;
+		if (logger == null) {
+			logger = freemind.main.Resources.getInstance().getLogger(
+					this.getClass().getName());
+		}
 	}
-	
+
 	public void show() {
 		// Make fields for short texts editable
 		textfield = (getText().length() < 8) ? new JTextField(getText(), 8)
 				: new JTextField(getText());
 
 		// Set textFields's properties
-
-		int cursorWidth = 1;
-		int xOffset = 0;
-		int yOffset = -1; // Optimized for Windows style; basically ad hoc
-		int widthAddition = 2 * 0 + cursorWidth + 2;
-		int heightAddition = 2;
-
-		// minimal width for input field of leaf or folded node (PN)
-		final int MINIMAL_LEAF_WIDTH = 150;
-		final int MINIMAL_WIDTH = 50;
-		final int MINIMAL_HEIGHT = 20;
 
 		final NodeView nodeView = getNode();
 		final MindMapNode model = nodeView.getModel();
@@ -117,9 +134,8 @@ public class EditNodeTextField extends EditNodeBase {
 			}
 		}
 
-		int ySize = nodeView.getMainView().getHeight()
-				+ heightAddition;
-		if(ySize < MINIMAL_HEIGHT) {
+		int ySize = nodeView.getMainView().getHeight() + heightAddition;
+		if (ySize < MINIMAL_HEIGHT) {
 			ySize = MINIMAL_HEIGHT;
 		}
 		textfield.setSize(xSize, ySize);
@@ -140,127 +156,8 @@ public class EditNodeTextField extends EditNodeBase {
 
 		// textField.selectAll(); // no selection on edit (PN)
 
-		final int EDIT = 1;
-		final int CANCEL = 2;
-		final Tools.IntHolder eventSource = new Tools.IntHolder();
-		eventSource.setValue(EDIT);
-
-		// listener class
-		class TextFieldListener implements
-				KeyListener, FocusListener, MouseListener, ComponentListener
-		{
-			private boolean checkSpelling = Resources.getInstance().
-						getBoolProperty(FreeMindCommon.CHECK_SPELLING);
-
-			public void focusGained(FocusEvent e) {
-			} // focus gained
-
-			public void focusLost(FocusEvent e) {
-				// %%% open problems:
-				// - adding of a child to the rightmost node
-				// - scrolling while in editing mode (it can behave just like
-				//   other viewers)
-				// - block selected events while in editing mode
-				if (! textfield.isVisible() || eventSource.getValue() == CANCEL) {
-					if (checkSpelling) {
-						eventSource.setValue(EDIT); // allow focus lost again
-					}
-					return;
-				}
-				if (e == null) { // can be when called explicitly
-					hideMe();
-					getEditControl().ok(textfield.getText());
-					eventSource.setValue(CANCEL); // disallow real focus lost
-				} else {
-					// always confirm the text if not yet
-					hideMe();
-					getEditControl().ok(textfield.getText());
-				}
-			}
-
-			public void keyPressed(KeyEvent e) {
-				// add to check meta keydown by koh 2004.04.16
-				if (e.isAltDown() || e.isControlDown() || e.isMetaDown()
-						|| eventSource.getValue() == CANCEL)
-				{
-					return;
-				}
-
-				boolean commit = true;
-
-				switch (e.getKeyCode()) {
-				case KeyEvent.VK_ESCAPE:
-					commit = false;
-				case KeyEvent.VK_ENTER:
-					e.consume();
-
-					eventSource.setValue(CANCEL);
-					hideMe();
-					// do not process loose of focus
-					if (commit) {
-						getEditControl().ok(textfield.getText());
-					} else {
-						getEditControl().cancel();
-					}
-					break;
-				case KeyEvent.VK_SPACE:
-					e.consume();
-				}
-			}
-
-			public void keyTyped(KeyEvent e) {
-			}
-
-			public void keyReleased(KeyEvent e) {
-			}
-
-			public void mouseClicked(MouseEvent e) {
-			}
-
-			public void mouseEntered(MouseEvent e) {
-			}
-
-			public void mouseExited(MouseEvent e) {
-			}
-
-			public void mousePressed(MouseEvent e) {
-				conditionallyShowPopup(e);
-			}
-
-			public void mouseReleased(MouseEvent e) {
-				conditionallyShowPopup(e);
-			}
-
-			private void conditionallyShowPopup(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					JPopupMenu popupMenu = new EditPopupMenu(textfield);
-					if (checkSpelling) {
-						popupMenu.add(SpellChecker.createCheckerMenu());
-						popupMenu.add(SpellChecker.createLanguagesMenu());
-						eventSource.setValue(CANCEL); // disallow real focus lost
-					}
-					popupMenu.show(e.getComponent(), e.getX(), e.getY());
-					e.consume();
-				}
-			}
-
-			public void componentHidden(ComponentEvent e) {
-				focusLost(null);
-			}
-
-			public void componentMoved(ComponentEvent e) {
-				focusLost(null);
-			}
-
-			public void componentResized(ComponentEvent e) {
-				focusLost(null);
-			}
-
-			public void componentShown(ComponentEvent e) {
-				focusLost(null);
-			}
-
-		}
+		mEventSource = new Tools.IntHolder();
+		mEventSource.setValue(EDIT);
 
 		// create the listener
 		final TextFieldListener textFieldListener = new TextFieldListener();
@@ -269,17 +166,53 @@ public class EditNodeTextField extends EditNodeBase {
 		this.textFieldListener = textFieldListener;
 		textfield.addKeyListener(textFieldListener);
 		textfield.addMouseListener(textFieldListener);
+		mUndoManager = new UndoManager();
+		textfield.getDocument().addUndoableEditListener(mUndoManager);
+		// Create an undo action and add it to the text component
+		textfield.getActionMap().put("Undo", new AbstractAction("Undo") {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (mUndoManager.canUndo()) {
+						mUndoManager.undo();
+					}
+				} catch (CannotUndoException e) {
+				}
+			}
+		});
+
+		// Bind the undo action to ctl-Z (or command-Z on mac)
+		textfield.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_Z, Toolkit
+						.getDefaultToolkit().getMenuShortcutKeyMask()), "Undo");
+
+		// Create a redo action and add it to the text component
+		textfield.getActionMap().put("Redo", new AbstractAction("Redo") {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (mUndoManager.canRedo()) {
+						mUndoManager.redo();
+					}
+				} catch (CannotRedoException e) {
+				}
+			}
+		});
+
+		// Bind the redo action to ctl-Y (or command-Y on mac)
+		textfield.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_Y, Toolkit
+						.getDefaultToolkit().getMenuShortcutKeyMask()), "Redo");
 
 		// screen positionining ---------------------------------------------
 
 		// SCROLL if necessary
 		getView().scrollNodeToVisible(nodeView, xExtraWidth);
 		Point mPoint = null;
-		if(mPoint==null) {
-			// NOTE: this must be calculated after scroll because the pane location
+		if (mPoint == null) {
+			// NOTE: this must be calculated after scroll because the pane
+			// location
 			// changes
 			mPoint = new Point();
-	
+
 			Tools.convertPointToAncestor(nodeView.getMainView(), mPoint,
 					mapView);
 			if (xExtraWidth < 0) {
@@ -307,6 +240,122 @@ public class EditNodeTextField extends EditNodeBase {
 		});
 	}
 
+	// listener class
+	class TextFieldListener implements KeyListener, FocusListener,
+			MouseListener, ComponentListener {
+		private boolean checkSpelling = Resources.getInstance()
+				.getBoolProperty(FreeMindCommon.CHECK_SPELLING);
+
+		public void focusGained(FocusEvent e) {
+		} // focus gained
+
+		public void focusLost(FocusEvent e) {
+			// %%% open problems:
+			// - adding of a child to the rightmost node
+			// - scrolling while in editing mode (it can behave just like
+			// other viewers)
+			// - block selected events while in editing mode
+			if (!textfield.isVisible() || mEventSource.getValue() == CANCEL) {
+				if (checkSpelling) {
+					mEventSource.setValue(EDIT); // allow focus lost again
+				}
+				return;
+			}
+			if (e == null) { // can be when called explicitly
+				hideMe();
+				getEditControl().ok(textfield.getText());
+				mEventSource.setValue(CANCEL); // disallow real focus lost
+			} else {
+				// always confirm the text if not yet
+				hideMe();
+				getEditControl().ok(textfield.getText());
+			}
+		}
+
+		public void keyPressed(KeyEvent e) {
+			// add to check meta keydown by koh 2004.04.16
+			// logger.info("Key " + e);
+			if (e.isAltDown() || e.isControlDown() || e.isMetaDown()
+					|| mEventSource.getValue() == CANCEL) {
+				return;
+			}
+
+			boolean commit = true;
+
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_ESCAPE:
+				commit = false;
+			case KeyEvent.VK_ENTER:
+				e.consume();
+
+				mEventSource.setValue(CANCEL);
+				hideMe();
+				// do not process loose of focus
+				if (commit) {
+					getEditControl().ok(textfield.getText());
+				} else {
+					getEditControl().cancel();
+				}
+				break;
+			case KeyEvent.VK_SPACE:
+				e.consume();
+			}
+		}
+
+		public void keyTyped(KeyEvent e) {
+		}
+
+		public void keyReleased(KeyEvent e) {
+		}
+
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+
+		public void mousePressed(MouseEvent e) {
+			conditionallyShowPopup(e);
+		}
+
+		public void mouseReleased(MouseEvent e) {
+			conditionallyShowPopup(e);
+		}
+
+		private void conditionallyShowPopup(MouseEvent e) {
+			if (e.isPopupTrigger()) {
+				JPopupMenu popupMenu = new EditPopupMenu(textfield);
+				if (checkSpelling) {
+					popupMenu.add(SpellChecker.createCheckerMenu());
+					popupMenu.add(SpellChecker.createLanguagesMenu());
+					mEventSource.setValue(CANCEL); // disallow real focus lost
+				}
+				popupMenu.show(e.getComponent(), e.getX(), e.getY());
+				e.consume();
+			}
+		}
+
+		public void componentHidden(ComponentEvent e) {
+			focusLost(null);
+		}
+
+		public void componentMoved(ComponentEvent e) {
+			focusLost(null);
+		}
+
+		public void componentResized(ComponentEvent e) {
+			focusLost(null);
+		}
+
+		public void componentShown(ComponentEvent e) {
+			focusLost(null);
+		}
+
+	}
+
 	protected void addTextfield() {
 		mParent.add(textfield, 0);
 	}
@@ -321,8 +370,11 @@ public class EditNodeTextField extends EditNodeBase {
 		textfield.removeFocusListener(textFieldListener);
 		textfield.removeKeyListener((KeyListener) textFieldListener);
 		textfield.removeMouseListener((MouseListener) textFieldListener);
-		mFocusListener.removeComponentListener((ComponentListener) textFieldListener);
-		// workaround for java bug http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7075600, see FreeMindStarter
+		mFocusListener
+				.removeComponentListener((ComponentListener) textFieldListener);
+		// workaround for java bug
+		// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7075600, see
+		// FreeMindStarter
 		parent.remove(textfield);
 		parent.revalidate();
 		parent.repaint(bounds);
