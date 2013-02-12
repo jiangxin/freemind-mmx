cb4425129197b87bf53a0b5213b0102b4f86ed51
Christian Foltin <christianfoltin@users.sourceforge.net>
2013-02-12 19:12:19 +0100

    * version increase.

--- freemind/freemind/main/FreeMind.java
+++ freemind/freemind/main/FreeMind.java
@@ -139,7 +139,7 @@ public class FreeMind extends JFrame implements FreeMindMain {
 
 	private Logger logger = null;
 
-	protected static final VersionInformation VERSION = new VersionInformation("1.0.0 RC 1");
+	protected static final VersionInformation VERSION = new VersionInformation("1.0.0 RC 2");
 
 	public static final String XML_VERSION = "1.0.1";
 
