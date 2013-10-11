6a02fdfa1da22eb36c8dc9f8d014176509e9a363
Christian Foltin <christianfoltin@users.sourceforge.net>
2013-10-11 11:47:54 +0200

    * First try for deactivate clone action

---
+++ freemind/accessories/plugins/DeactivateCloneAction.java
@@ -0,0 +1,64 @@
+/*FreeMind - A Program for creating and viewing Mindmaps
+ *Copyright (C) 2000-2013 Joerg Mueller, Daniel Polansky, Christian Foltin, Dimitri Polivaev and others.
+ *
+ *See COPYING for Details
+ *
+ *This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package accessories.plugins;
+
+import javax.swing.Action;
+import javax.swing.JMenuItem;
+
+import accessories.plugins.ClonePasteAction.Registration;
+import freemind.controller.MenuItemEnabledListener;
+import freemind.modes.MindMapNode;
+import freemind.modes.mindmapmode.hooks.MindMapNodeHookAdapter;
+
+/**
+ * @author foltin
+ * @date 11.10.2013
+ */
+public class DeactivateCloneAction extends MindMapNodeHookAdapter implements MenuItemEnabledListener {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * freemind.extensions.NodeHookAdapter#invoke(freemind.modes.MindMapNode)
+	 */
+	public void invoke(MindMapNode pNode) {
+		super.invoke(pNode);
+		ClonePlugin hook = ClonePlugin.getHook(pNode);
+		if (hook != null) {
+			// double add to remove:
+			hook.toggleHook();
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see freemind.controller.MenuItemEnabledListener#isEnabled(javax.swing.JMenuItem, javax.swing.Action)
+	 */
+	public boolean isEnabled(JMenuItem pItem, Action pAction) {
+		return getRegistration().isEnabled(pItem, pAction); 
+	}
+	
+	protected Registration getRegistration() {
+		return (Registration) getPluginBaseClass();
+	}
+
+}
