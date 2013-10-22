810315cf86adaa822dcd41a931ee94df2c27133a
Christian Foltin <christianfoltin@users.sourceforge.net>
2013-10-22 22:38:34 +0200

    * double check corrected

--- freemind/check_for_duplicate_resources.sh
+++ freemind/check_for_duplicate_resources.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for s in Res*.properties ; do 
 #    echo ">>>>>>>>>>>>>>> Start $s" ; 
-    for g in $(cat $s | cut -d '=' -f 1 | grep -v "^\\s*#" | grep -v "^\\s*$" | sort | uniq -d) ; do 
-	echo "=======    File: $s, key $g"
+    for g in $(cat $s | cut -d '=' -f 1 | grep -v "^[[:space:]]*#" | grep -v "^[[:space:]]*$" | sort | uniq -d) ; do 
+	echo "=======    File: $s, key '$g'"
 	grep -n "^$g\\s*=" $s
     done ; 
 #    echo "<<<<<<<<<<<<<<< End $s" ; 
